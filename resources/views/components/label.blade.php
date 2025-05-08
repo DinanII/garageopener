9b2eb9348ab7f4793cda0ee137601fb66f7ee4c1
Dinan <dinanvanw@gmail.com>
2025-05-08 19:00:12 +0200

    Finalized label component setup

--- resources/views/components/label.blade.php
+++ resources/views/components/label.blade.php
@@ -1,8 +1,11 @@
 @props([
   'containsBorder' => true,
-  'textContent' => 'Label text'
+  'textContent' => 'Label text',
+  'fontSize' => 'text-[0.750rem]',
 ])
-<div class="rounded-full bg-white flex space-x-* p-3" >
- <p class="bg-accentPrimary p-1 rounded-full h-0.5"></p>
- <p>{{ $textContent }}</p>
-</div>
+<span class="rounded-full bg-white flex space-x-* p-2 w-fit h-fit items-center justify-center {{ $containsBorder == true ? 'border-borderAccent border-2 ' : '' }}" >
+<!-- Decorational circle -->
+<p class="bg-accentPrimary p-1 rounded-full h-0.5 mr-2"></p>
+<!-- Text with custom font size -->
+<p class="{{ $fontSize }} font-bold" >{{ $textContent }}</p>
+</span>
