ecbc08383880afef4d880638d86ae6570164970f
Dinan <dinanvanw@gmail.com>
2025-05-08 20:55:53 +0200

    Content finalization TextBlock

--- resources/views/components/textblock.blade.php
+++ resources/views/components/textblock.blade.php
@@ -1,12 +1,25 @@
 @props([
-  'labelText' => '',
+  'labelText' => 'Label',
   'mainTitle' => 'This is a large title',
+  'mainTitleClasses' => '',
   'description' => 'This is a longer description for this textblock.',
-  'primaryButtonText' => '',
+  'primaryButtonText' => 'DISPLAY',
   'primaryButtonIcon' => '',
-  'secondaryButtonText' => '',
+  'secondaryButtonText' => 'DISPLAY',
   'secondaryButtonIcon' => ''
 ])
-<div>
-<x-label></x-label>
+<div class="p-13" >
+  <x-label :textContent="$labelText"></x-label>
+  <!-- In case of a conflict (i.e. mainTitleClasses contains text-2xl), the last class will overrule all classes stated before -->
+  <h3 class="font-extrabold text-8xl max-w-7/12 h-fit {{ $mainTitleClasses }}" >{{ $mainTitle }}</h3>
+  <p class='my-6 px-5' >{{ $description }}</p>
+  <div class="flex" >
+  @if($primaryButtonText !== 'NO_DISPLAY')
+  <x-button onclick="myFunction()" :primary="true" :text="$primaryButtonText"></x-button>
+  @endif
+  @if($secondaryButtonText !== 'NO_DISPLAY')
+  <x-button :primary="false" :text="$primaryButtonText"></x-button>
+  @endif
+  </div>
+
 </div>
