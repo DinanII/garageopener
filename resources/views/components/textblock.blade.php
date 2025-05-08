a03b286e9819888dfdc0c9a5f019eb13a487f973
Dinan <dinanvanw@gmail.com>
2025-05-08 20:05:09 +0200

    Created textblock component file with props

---
+++ resources/views/components/textblock.blade.php
@@ -0,0 +1,9 @@
+@props([
+  'labelText' => '',
+  'mainTitle' => 'This is a large title',
+  'description' => 'This is a longer description for this textblock.',
+  'primaryButtonText' => '',
+  'primaryButtonIcon' => '',
+  'secondaryButtonText' => '',
+  'secondaryButtonIcon' => ''
+])
