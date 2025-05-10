fec1c9f17d52b22401af96af8d272960ef5c7433
Dinan <dinanvanw@gmail.com>
2025-05-10 22:34:06 +0200

    TextBlock: Cleanup and button icon display

--- resources/views/components/textblock.blade.php
+++ resources/views/components/textblock.blade.php
@@ -10,15 +10,18 @@
 ])
 <div class="p-13" >
   <x-label :textContent="$labelText"></x-label>
-  <!-- In case of a conflict (i.e. mainTitleClasses contains text-2xl), the last class will overrule all classes stated before -->
   <h3 class="font-extrabold max-w-7/12 h-fit {{ $mainTitleClasses }}" >{{ $mainTitle }}</h3>
   <p class='my-6 px-5' >{{ $description }}</p>
   <div class="flex" >
   @if($primaryButtonText !== 'NO_DISPLAY')
-  <x-button onclick="myFunction()" :primary="true" :text="$primaryButtonText"></x-button>
+  <x-button :primary="true" :text="$primaryButtonText">
+    {!! $primaryButtonIcon !!}
+  </x-button>
   @endif
   @if($secondaryButtonText !== 'NO_DISPLAY')
-  <x-button :primary="false" :text="$primaryButtonText"></x-button>
+  <x-button :primary="false" :text="$primaryButtonText">
+    {!! $secondaryButtonIcon !!}
+  </x-button>
   @endif
   </div>
 
