ed01547837a8a2710ff69c5ee0b1489ed7a9bfcd
Dinan <dinanvanw@gmail.com>
2025-05-11 01:30:37 +0200

    Initial version of Image-TextBlock section

--- resources/views/components/textblock.blade.php
+++ resources/views/components/textblock.blade.php
@@ -1,17 +1,18 @@
 @props([
   'labelText' => 'Label',
   'mainTitle' => 'This is a large title',
-  'mainTitleClasses' => 'text-8xl',
+  'mainTitleClasses' => '',// text-8xl
   'description' => 'This is a longer description for this textblock.',
   'primaryButtonText' => 'NO_DISPLAY',
   'primaryButtonIcon' => '',
   'secondaryButtonText' => 'NO_DISPLAY',
-  'secondaryButtonIcon' => ''
+  'secondaryButtonIcon' => '',
+  'buttonClasses' => ''
 ])
 <div class="p-13" >
   <x-label :textContent="$labelText"></x-label>
   <h3 class="font-extrabold max-w-7/12 h-fit {{ $mainTitleClasses }}" >{{ $mainTitle }}</h3>
-  <p class='my-6 px-5 max-w-7/12' >{{ $description }}</p>
+  <p class='my-6 max-w-7/12' >{{ $description }}</p>
   <div class="flex" >
   @if($primaryButtonText !== 'NO_DISPLAY')
   <x-button :primary="true" :text="$primaryButtonText">
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,9 +15,10 @@
   <section class="flex p-2">
     <x-card cardTitle="Effortless Security"
     cardDescription="Unlock peace of mind with our garage door opener, effortless convenience coupled with robust security at the touch of a button.">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#00D1B2" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-  <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
-</svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#00D1B2" class="bi bi-geo-alt-fill"
+      viewBox="0 0 16 16">
+      <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6" />
+    </svg>
 
     </x-card>
     <x-card cardTitle="Seamless Integration"
@@ -32,11 +33,47 @@
     </x-card>
     <x-card cardTitle="Reliability Redefined"
     cardDescription="Trust in consistent performance, our garage door opener is engineered for enduring reliability, simplifying your daily routine.">
-    <svg class="w-2/3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="#00cfb3" d="M0 488L0 171.3c0-26.2 15.9-49.7 40.2-59.4L308.1 4.8c7.6-3.1 16.1-3.1 23.8 0L599.8 111.9c24.3 9.7 40.2 33.3 40.2 59.4L640 488c0 13.3-10.7 24-24 24l-48 0c-13.3 0-24-10.7-24-24l0-264c0-17.7-14.3-32-32-32l-384 0c-17.7 0-32 14.3-32 32l0 264c0 13.3-10.7 24-24 24l-48 0c-13.3 0-24-10.7-24-24zm488 24l-336 0c-13.3 0-24-10.7-24-24l0-56 384 0 0 56c0 13.3-10.7 24-24 24zM128 400l0-64 384 0 0 64-384 0zm0-96l0-80 384 0 0 80-384 0z"/></svg>
+    <svg class="w-2/3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+      <path fill="#00cfb3"
+      d="M0 488L0 171.3c0-26.2 15.9-49.7 40.2-59.4L308.1 4.8c7.6-3.1 16.1-3.1 23.8 0L599.8 111.9c24.3 9.7 40.2 33.3 40.2 59.4L640 488c0 13.3-10.7 24-24 24l-48 0c-13.3 0-24-10.7-24-24l0-264c0-17.7-14.3-32-32-32l-384 0c-17.7 0-32 14.3-32 32l0 264c0 13.3-10.7 24-24 24l-48 0c-13.3 0-24-10.7-24-24zm488 24l-336 0c-13.3 0-24-10.7-24-24l0-56 384 0 0 56c0 13.3-10.7 24-24 24zM128 400l0-64 384 0 0 64-384 0zm0-96l0-80 384 0 0 80-384 0z" />
+    </svg>
 
     </x-card>
   </section>
-
+  <!-- TextBlock & image sections -->
+  <!-- Section 1/4-->
+  <section class="flex justify-between w-11/12 mx-auto">
+    <x-textblock class="w-1/2" labelText="Section"
+    description="At Lyfter, we make smart devices to automate your home. Convenient, secure and customizable."
+    mainTitle="Everyday Convenience" primaryButtonText="Buy Lyfter Garage" mainTitleClasses="text-4xl"
+    secondaryButtonText="Download the app"></x-textblock>
+    <img class="w-1/3" src="https://picsum.photos/600/600">
+  </section>
+  <!-- Section 2/4-->
+  <section class="flex justify-between w-11/12 mx-auto">
+    <img class="w-1/3" src="https://picsum.photos/600/600">
+    <x-textblock class="w-1/2" labelText="Section"
+    description="At Lyfter, we make smart devices to automate your home. Convenient, secure and customizable."
+    mainTitle="Everyday Convenience" primaryButtonText="Buy Lyfter Garage" mainTitleClasses="text-4xl"
+    secondaryButtonText="Download the app"></x-textblock>
+  </section>
+    <!-- Section 3/4-->
+    <section class="flex justify-between ml-0">
+    <x-textblock class="w-1/2" labelText="Section"
+    description="At Lyfter, we make smart devices to automate your home. Convenient, secure and customizable."
+    mainTitle="Everyday Convenience" primaryButtonText="Buy Lyfter Garage" mainTitleClasses="text-4xl"
+    secondaryButtonText="Download the app"></x-textblock>
+    <img class="w-1/3" src="https://picsum.photos/600/600">
+  </section>
+  <!-- Section 4/4-->
+  <section class="flex justify-between">
+    <img class="w-1/3" src="https://picsum.photos/600/600">
+    <x-textblock class="w-1/2" labelText="Section"
+    description="At Lyfter, we make smart devices to automate your home. Convenient, secure and customizable."
+    mainTitle="Everyday Convenience" primaryButtonText="Buy Lyfter Garage" mainTitleClasses="text-4xl"
+    secondaryButtonText="Download the app"></x-textblock>
+  </section>
+  <!-- Loose Components -->
   <x-button background="bg-transparent">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-arrow-up-right"
     viewBox="0 0 16 16">
