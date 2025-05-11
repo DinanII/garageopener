7c3a9ddcbfd2281a9471d70f46dc243532187e71
Dinan <dinanvanw@gmail.com>
2025-05-11 21:20:45 +0200

    Header size/layout changes

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -2,13 +2,21 @@
   <!-- Content Wrapper -->
   <div class="w-11/12 mx-auto flex justify-between py-3">
     <img src="{{ get_template_directory_uri() }}/resources/images/LyfterLogo.png" alt="Lyfter Logo" class="h-1/3">
-    <div class="flex justify-around w-5/12 h-fit">
+    <div class="flex justify-around w-6/12 h-fit">
       <div class="flex">
         <x-button fSize="text-sm" text="Lyfter Garage" background="bg-transparent"></x-button>
         <x-button fSize="text-sm" text="Lyfter Doorbell" background="bg-transparent"></x-button>
       </div>
       <div class="flex">
-        <x-button fSize="text-sm" text="Support" background="bg-transparent"></x-button>
+        <x-button fSize="text-sm" text="Support" background="bg-transparent">\
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-chat-dots"
+            viewBox="0 0 16 16">
+            <path
+              d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
+            <path
+              d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9 9 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.4 10.4 0 0 1-.524 2.318l-.003.011a11 11 0 0 1-.244.637c-.079.186.074.394.273.362a22 22 0 0 0 .693-.125m.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6-3.004 6-7 6a8 8 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a11 11 0 0 0 .398-2" />
+          </svg>
+        </x-button>
         <x-button fSize="text-sm" text="Download the app" primary="false">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-download"
             viewBox="0 0 16 16">
